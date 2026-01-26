8b42087e28abe57591d2d90cc2c0e55a6202df39
brenofelips <brendo.filipe2050@outlook.com>
2026-01-26 18:33:47 -0300

    feat: ajustando o minha entitie e criando um novo enum

--- src/main/java/com/dev/java/EventClean/core/entities/Event.java
+++ src/main/java/com/dev/java/EventClean/core/entities/Event.java
@@ -1,15 +1,15 @@
 package com.dev.java.EventClean.core.entities;
 
+import com.dev.java.EventClean.core.enums.TipoEvento;
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-public record Event(
-    UUID id,
-    String descricao,
-    LocalDateTime inicio,
-    LocalDateTime fim,
-    String identificador,
-    String local,
-    Integer capacidade,
-    String tipoEvento
+public record Event(UUID id,
+                    String descricao,
+                    String identificador,
+                    LocalDateTime inicio,
+                    LocalDateTime fim,
+                    String local,
+                    Integer capacidade,
+                    TipoEvento tipoEvento
 ) {}
---
+++ src/main/java/com/dev/java/EventClean/core/enums/TipoEvento.java
@@ -0,0 +1,9 @@
+package com.dev.java.EventClean.core.enums;
+
+public enum TipoEvento {
+  WORKSHOP,
+  PALESTRA,
+  MUSICA,
+  SEMINARIO,
+  CONFERENCIA
+}
